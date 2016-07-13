049157b74affd2e34fc8d040625b6fb45ab88056
Alma Wang <almawang96@gmail.com>
2016-07-13 02:02:38 -0400

    finished SA

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 // es6 import style
-import $ from 'jquery';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
+import youtubeSearch from './youtube-api';
+import debounce from 'lodash.debounce';
 
 import './style.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 300);
+    this.search('pixar');
+  }
+  search(text) {
+    youtubeSearch(text).then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+  render() {
+    return (
+      <div id="app">
+        <SearchBar onSearchChange={text => this.search(text)} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 
 // this is a generator function.
 // note the *
-function *simpleCounter() {
+/*function *simpleCounter() {
   let count = 0;
   while (true) {
     yield count++;
@@ -20,4 +60,4 @@ setInterval(
   // arrow notation for anonymous function
   () => { $('#main').html(`You've been on this page for ${counter.next().value} seconds.`); }
   , 1000
-);
+);*/
